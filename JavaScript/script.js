4c98f3166d63bd678fddfacc471267fc7d025cf3
George Katsaounis <Katsa@Katsas-Mac.local>
2016-03-14 12:17:20 -0400

    first JS

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,13 @@
 // Check if the user is ready to play!
 
-confirm("This is to confirm if the user is ready");
+confirm("This is to confirm if the user is ready"); //popup with message to indicate if user is ready
+var age = prompt("What's your age"); //popup asking user their age
+
+if(age < 13) {
+    console.log("You can play, but I have responsibility"); //print to console
+
+}
+else {
+console.log("Go paly");
+
+}
